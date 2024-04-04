c5aa663c44a88f4fecbc9383f9cb0d24c2ec993c
Jiang <13131892163@163.com>
2024-04-04 20:04:25 +0800

    init ddd project

---
+++ chatbot-ai/chatbot-api-interfaces/src/main/java/com/jr/ai/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.jr.ai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: JiangJiang
+ * description API启动类
+ * date: 2024/4/3 18:35
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class , args);
+    }
+}
---
+++ chatbot-ai/chatbot-api-interfaces/src/test/java/com/jr/ai/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package com.jr.ai.api.test;
+
+/**
+ * author: JiangJiang
+ * description 单元测试
+ * date: 2024/4/4 19:57
+ */
+public class ApiTest {
+}
